2c1a243cbbb17f97b08423e0afc8b71d6770e130
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-12-20 22:04:49 +0900

    #1.5

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(210 - 30, 200 - 20, 15, 100);
+ctx.fillRect(350 - 45, 200 - 20, 15, 100);
+ctx.fillRect(260 - 40, 200 - 20, 60, 150);
+ctx.fillRect(260 - 40, 350, 15, 150);
+ctx.fillRect(265, 350, 15, 150);
+
+ctx.arc(250, 110, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(260 + 10, 95, 6, 0, 2 * Math.PI);
+ctx.arc(220 + 10, 95, 6, 0, 2 * Math.PI);
+ctx.fillStyle = "white";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(250, 120, 30, 0, Math.PI);
+ctx.fillStyle = "white";
 ctx.fill();
